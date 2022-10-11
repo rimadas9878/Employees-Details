76e0c62f93c3a865bb8947eb148ad432c445e25e
Rima Das <rimadas.automationtester@gmail.com>
2022-10-10 23:44:27 -0700

    Adding Manager test

---
+++ test/manager.test.js
@@ -0,0 +1,28 @@
+const Manager = require('../lib/Manager');
+
+describe("Manager", () => {
+    describe("Initialization", () => {
+        it("Should be able to display manager office number", () =>{
+            const value = 12345;
+            const manager = new Manager("rima", 1, "abc@test.com",value);
+            expect(manager.gitHub).toBe("value");
+        });
+
+        describe("getOfficeNumber", () => {
+            it("Should be able to get phone number", () => {
+                const value = 12345;
+                const manager = new Manager("rima", 1, "abc@test.com", value);
+                expect(manager.getOfficeNumber()).toBe(value);
+            });
+        });
+
+        describe("getRole", () => {
+            it("Should be able to add content as per role", () => {
+                const value = "Manager";
+                const manager = new Manager("rima", 1, "abc@test.com", 12345);
+                expect(manager.getRole()).toBe(value);
+            });
+        });
+
+    });
+});
